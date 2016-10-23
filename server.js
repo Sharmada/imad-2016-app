f016dd2573949334edd18d75229349187c81408a
Sharmada <sharmadasankar@gmail.com>
2016-10-23 08:05:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,30 +4,46 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-var articleOne = {
+var articles = {
+  articleOne : {
     title : 'Article-one | sharmada' ,
     heading : 'Article-one',
-    date : 'oct 22,2016',
+    date : 'oct 5,2016',
     content : `
                 <p> Hello everyone. This is article one and by reading this text you can do nothing but just waste time :P </p>
                 <p> So,please do not take much time to read this. </p>
                 <p>As i am running in short of time this article will be a complete mess.Later, it will be build in such a way that it makes atleast a little bit of sense ;)</p>
     `
-       };
+       },
+  articleTwo : {
+      title : 'Article-two | sharmada' ,
+    heading : 'Article-two',
+    date : 'oct 18,2016',
+    content : `
+                <p> Hello everyone. This is article two and by reading this text you can do nothing but just waste time :P </p>
+    `
+  },
+  articleThree : {
+      title : 'Article-three | sharmada' ,
+    heading : 'Article-three',
+    date : 'oct 10,2016',
+    content : `
+                <p> Hello everyone. This is article three that you are reading. </p>
+                `}
+};
      function createTemplate (data) {
         var title = data.title;
         var content = data.content;
         var date = data.date;
         var heading = data.heading;
-    var htmlTemplate = { `
+    var htmlTemplate = `
         <html>
-     <head>
-        <title>
+          <head>
+            <title>
              ${title}
-        </title>
-        <meta name="viewport" content= "width=device-width , initial-scale=1" />
-         <link href="/ui/style.css" rel="stylesheet" />
+            </title>
+            <meta name="viewport" content= "width=device-width , initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
         </head>
         <body>
             <div class ="container">
@@ -41,7 +57,7 @@ var articleOne = {
             </div>
         </body>
 </html>
-` ;
+`;
 return htmlTemplate;
 }
     app.get('/', function (req, res) {
@@ -63,8 +79,6 @@ return htmlTemplate;
     app.get('/ui/madi.png', function (req, res) {
       res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
     });
-
-
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
